32ed47a8c89ff85387d168c33bed22159c62bf8c
Anthony Maida <ajajmaida@gmail.com>
2022-05-11 14:03:46 -0400

    Added kernel density

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
-import load
-from collections import Counter
-import math
+import matplotlib.pyplot as plt
 import numpy as np
+import load
 
-def counter_cosine_similarity(c1, c2):
-    terms = set(c1).union(c2)
-    dotprod = sum(c1.get(k, 0) * c2.get(k, 0) for k in terms)
-    magA = math.sqrt(sum(c1.get(k, 0)**2 for k in terms))
-    magB = math.sqrt(sum(c2.get(k, 0)**2 for k in terms))
-    return dotprod / (magA * magB)
+def n_ln(dist, numbins):
+    log_dist = np.log10(dist)
+    bins = np.linspace(min(log_dist),max(log_dist), numbins)
+    hist, r_array = np.histogram(log_dist, bins)
 
-data = load.LoadLogFile()
+    dR = r_array[1]-r_array[0]    
+    x_array = r_array[1:] - dR/2
+    volume =  [4.*np.pi*i**3. for i in 10**x_array[:] ]
 
-counterA = Counter(data[0])
-counterB = Counter(data[1])
+    return [10**x_array, hist/dR/volume]
 
-print(counterA)
-print(counterB)
-print(counter_cosine_similarity(counterA, counterB))
+plt.style.use('seaborn-whitegrid')
+
+data = np.array(load.LoadLogFile())
+result = n_ln(data[:,1], 284200)
+print(result)
+
+#here's our data to plot, all normal Python lists
+x = data[:,1]
+y = np.full((284200, 1), 0)
+
+#intensity = np.full((284200, 1), 1)
+
+#setup the 2D grid with Numpy
+#x, y = np.meshgrid(x, y)
+
+#now just plug the data into pcolormesh, it's that easy!
+#plt.pcolormesh(x, y, intensity)
+#plt.colorbar() #need a colorbar to show the intensity scale
+plt.plot(x,y)
+plt.show() #boom
